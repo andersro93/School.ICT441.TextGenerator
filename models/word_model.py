f43565a630bf2482513d319821c2243bb8641c6c
Anders Refsdal Olsen <andersro93@ros-nett.com>
2018-05-25 10:14:36 +0200

    Implemented word prediction Added some files to .gitignore

--- models/word_model.py
+++ models/word_model.py
@@ -1,21 +1,107 @@
+from typing import Dict, Any
+
+import numpy
 from keras import Sequential
-from keras.layers import LSTM, Dense
+from keras.callbacks import ModelCheckpoint
+from keras.layers import LSTM, Dense, Dropout, Bidirectional
+from keras.utils import np_utils
 
 from .model import Model
 
 
 class WordModel(Model):
     """
-    Word model that is used to predict whole next words
+    Simple model for predicting single characters
     """
+
+    data_x: numpy.ndarray = None
+    """ The train data """
+
+    data_y: numpy.ndarray = None
+    """ The output data """
+
+    pattern_length: int = 10
+    """ The total length of each training pattern """
+
+    __word_to_int_dictionary: Dict[str, int] = None
+    """ The dictionary containing the words with their integer value """
+
+    __unique_words: set = None
+    """ A set off all the unique words """
 
     def __init__(self):
         """
-        Word model constructor
+        CharacterModel constructor
         """
         super(WordModel, self).__init__()
 
         self._model_path = "./generated_models/word-{epoch:02d}-{loss:.4f}.hdf5"
+
+    def prepare_model(self, print_info=False):
+        """
+        Prepares the model, parses data etc..
+        :type print_info: bool, Prints information about parsing if true
+        :return: None
+        """
+        self._read_data_from_assets()
+        self._concat_assets_content_to_one_string()
+
+        self.__unique_words = sorted(list(set(self._raw_content.split())))
+        self.__word_to_int_dictionary = dict((word, index) for index, word in enumerate(self.__unique_words))
+
+        data_x, data_y = self._format_data_to_x_and_y()
+
+        amount_of_patterns = len(data_x)
+
+        # Assign the data to class
+        self.data_x = numpy.reshape(data_x, (amount_of_patterns, self.pattern_length, 1))
+        self.data_y = np_utils.to_categorical(data_y)
+
+        if print_info:
+            print(f"Total length: {len(self._raw_content)}")
+            print(f"Words: {len(self.__unique_words)}")
+            print(f"Total Patterns: {amount_of_patterns}")
+
+    def generate_text_of_length(self, length: int, verbose=False) -> str:
+        """
+        Predicts and prints from the given phrase.
+        The amount of characters to predict is decided by the parameter length
+        :param length: The amount of characters to predict
+        :param verbose: Boolean value for verbose generation or not
+        :return: str
+        """
+        # String to return
+        return_string = ''
+
+        # Pick a random pattern to use for prediction
+        pattern_id = numpy.random.randint(0, len(self.data_x) - 1)
+
+        # Get the selected pattern
+        pattern = self.data_x[pattern_id]
+
+        if verbose:
+            print('Seed used to print:')
+            print(''.join(self.__get_word_from_number(value) for value in pattern))
+
+        # Predict the given times
+        for i in range(length):
+            # Format the current phrase into correct vector
+            x_input = numpy.reshape(pattern, (1, len(pattern), 1))
+
+            # Get a prediction
+            predictions = self._model.predict(x_input)
+
+            # Obtain the predicted word
+            predicted_int_for_word = numpy.random.choice(len(predictions[0]), p=predictions[0])
+
+            # Append the translated letter to the return string
+            return_string += self.__get_word_from_number(predicted_int_for_word)
+
+            # Add the letter to the next prediction
+            pattern = numpy.append(pattern, predicted_int_for_word)
+            pattern = pattern[1:len(pattern)]
+
+        return return_string
 
     def create_model(self) -> None:
         """
@@ -24,14 +110,62 @@ class WordModel(Model):
         """
         model = Sequential()
 
-        # First layer
-        model.add(
-            LSTM(32, activation='relu')
-        )
+        model.add(LSTM(256, input_shape=(self.data_x.shape[1], self.data_x.shape[2]), return_sequences=True))
+        model.add(Dropout(0.2))
 
-        # Second layer
-        model.add(
-            Dense(32, activation='relu')
-        )
+        model.add(Bidirectional(LSTM(256)))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(self.data_y.shape[1], activation='softmax'))
 
         self._model = model
+
+    def train_model(self, epochs=30, batch_size=150) -> None:
+        """
+        Starts training the model
+        :param epochs: int
+        :param batch_size: int
+        :return: None
+        """
+
+        self.compile_model()
+
+        checkpoint = ModelCheckpoint(self._model_path, monitor='loss', verbose=1, save_best_only=True, mode='min')
+        callbacks_list = [checkpoint]
+
+        self._model.fit(
+            x=self.data_x,
+            y=self.data_y,
+            batch_size=batch_size,
+            epochs=epochs,
+            callbacks=callbacks_list
+        )
+
+    def __get_word_from_number(self, number: numpy.ndarray) -> str:
+        """
+        Helper method for finding the letter for a corresponding number
+        :param number:
+        :return: str:
+        """
+        for word in self.__word_to_int_dictionary:
+            if self.__word_to_int_dictionary[word] == number:
+                return word
+
+        return f'Could not find word for number: {number}'
+
+    def _format_data_to_x_and_y(self) -> tuple:
+        """
+        Formats the data to an x and y dictionary
+        :return: tuple:
+        """
+        data_x = []
+        data_y = []
+        for i in range(0, len(self._raw_content) - self.pattern_length, 1):
+
+            seq_x = self._raw_content[i:i + self.pattern_length]
+            seq_y = self._raw_content[i + self.pattern_length]
+
+            data_x.append([self.__word_to_int_dictionary[word] for word in seq_x])
+            data_y.append(self.__word_to_int_dictionary[seq_y])
+
+        return data_x, data_y
